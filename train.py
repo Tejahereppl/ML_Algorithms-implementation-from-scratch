ea6250bff20ea9684ce1f2b426ddae278087af60
Tejahereppl <137330578+Tejahereppl@users.noreply.github.com>
2024-12-25 17:26:56 +0530

    Add files via upload

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from DecisionTree import DecisionTree
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from Random_Forest import RandomForest
 import numpy as np
 import pandas as pd
 
@@ -15,14 +16,18 @@ X = df.drop(columns = ["Air Quality"]).to_numpy()
 
 X_train,X_test,Y_train,Y_test = train_test_split(X,y,test_size = 0.2)
 
-tree = DecisionTree()
-tree.fit(X_train,Y_train)
-y_pred = tree.predict(X_test)
+decision_tree = DecisionTree()
+Random_forest = RandomForest()
+decision_tree.fit(X_train,Y_train)
+Random_forest.fit(X_train,Y_train)
+y_pred1 = decision_tree.predict(X_test)
+y_pred2 = Random_forest.predict(X_test)
 
 def accuracy(y_pred,y_test):
     return np.sum(y_pred==y_test)/len(y_test)
 
-print(accuracy(y_pred,Y_test))
+print("Accuracy for Decision Tree" + str(accuracy(y_pred1,Y_test)))
+print("Accuracy for Random Forest" + str(accuracy(y_pred2,Y_test)))
 
 
 
